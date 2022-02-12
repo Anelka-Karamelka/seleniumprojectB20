8c94d1fe84eb1f689f082675ee66b05b1492603f
Anelya <ssmanelya18@gmail.com>
2022-02-12 10:42:48 -0500

    Avengers Practice hours

---
+++ AvengerPracticeHour/src/test/java/com/cydeo/tests/day02_02_12_2022/TC02.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day02_02_12_2022;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class TC02 {
+
+    public static void main(String[] args) {
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+    }
+}
