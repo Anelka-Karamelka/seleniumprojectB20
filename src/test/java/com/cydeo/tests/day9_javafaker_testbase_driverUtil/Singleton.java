d3f5957e3e409a56865a2c9d6bd218b40e72b74a
Anelya <ssmanelya18@gmail.com>
2022-03-10 11:24:25 -0500

    day9 _java faker

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/Singleton.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+public class Singleton {
+
+
+    //create private constructor
+
+    private Singleton(){}
+   // create private static String
+        // prevent access and provide a getter method
+
+
+        private static String word;
+
+        public static String getWord(){
+
+            if (word == null){
+                System.out.println("First time call. Word object is null. " +
+                        "Assigning value to it now!");
+                word = "something";
+            }else{
+                System.out.println("Word already has value");
+
+            }
+
+            return word;
+
+}
+}
