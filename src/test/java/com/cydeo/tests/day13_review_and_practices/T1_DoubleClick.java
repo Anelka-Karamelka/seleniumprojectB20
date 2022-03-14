cf806d9cefdc491880c907bc96053af82e9b68ef
Anelya <ssmanelya18@gmail.com>
2022-03-14 18:15:34 -0400

    day13_Review

--- src/test/java/com/cydeo/tests/day13_review_and_practices/T1_DoubleClick.java
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/T1_DoubleClick.java
@@ -1,6 +1,9 @@
 package com.cydeo.tests.day13_review_and_practices;
 
+import com.cydeo.pages.DoubleClickPage;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T1_DoubleClick {
@@ -13,9 +16,21 @@ public class T1_DoubleClick {
 //        https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2
         Driver.getDriver().get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2");
 //        2. Switch to iframe.
+        Driver.getDriver().switchTo().frame("iframeResult");
 //        3. Double-click on the text “Double-click me to change my text color.”
+        DoubleClickPage doubleClickPage=new DoubleClickPage();// this line calling POM, calling constructor
+
+        Actions actions=new Actions(Driver.getDriver()); // this line gives allows to double-click
+
+
+        actions.doubleClick(doubleClickPage.textToDoubleClick).perform();
 //        4. Assert: Text’s “style” attribute value contains “red”.
-//
-//        NOTE: FOLLOW POM
+        System.out.println("doubleClickPage.textToDoubleClick.getAttribute(\"style\") = "
+                + doubleClickPage.textToDoubleClick.getAttribute("style"));
+        String actualAttributeValue= doubleClickPage.textToDoubleClick.getAttribute("style");
+        String expectedInStyleAttribute="red";
+
+        Assert.assertTrue(actualAttributeValue.contains(expectedInStyleAttribute));
+
     }
 }
